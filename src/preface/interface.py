993e10107024c418dbed3b9591b88ee453724a58
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-04 23:47:11 +0100

    Update src/preface/interface.py

--- src/preface/interface.py
+++ src/preface/interface.py
@@ -3,6 +3,7 @@ import typing
 
 import boto3
 
+import config
 import src.elements.s3_parameters as s3p
 import src.elements.service as sr
 import src.functions.cache
@@ -18,10 +19,20 @@ class Interface:
     """
 
     def __init__(self):
-        pass
 
-    @staticmethod
-    def exc() -> typing.Tuple[boto3.session.Session, s3p.S3Parameters, sr.Service]:
+        self.__configurations = config.Config()
+
+    def __arguments(self, connector: boto3.session.Session) -> dict:
+        """
+
+        :param connector:
+        :return:
+        """
+
+        return src.s3.configurations.Configurations(connector=connector).objects(
+            key_name=self.__configurations.arguments_key)
+
+    def exc(self) -> typing.Tuple[boto3.session.Session, s3p.S3Parameters, sr.Service, dict]:
         """
 
         :return:
@@ -32,7 +43,9 @@ class Interface:
         service: sr.Service = src.functions.service.Service(
             connector=connector, region_name=s3_parameters.region_name).exc()
 
+        arguments = self.__arguments(connector=connector)
+
         # Setting up the cloud storage area
         src.preface.setup.Setup(service=service, s3_parameters=s3_parameters).exc()
 
-        return connector, s3_parameters, service
+        return connector, s3_parameters, service, arguments
