3cc873ea5cf2c0db8b69f4fb6ee332e051e1da8b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-04 21:54:14 +0100

    Update src/preface/interface.py

--- src/preface/interface.py
+++ src/preface/interface.py
@@ -7,6 +7,7 @@ import src.elements.s3_parameters as s3p
 import src.elements.service as sr
 import src.functions.cache
 import src.functions.service
+import src.preface.setup
 import src.s3.configurations
 import src.s3.s3_parameters
 
@@ -30,5 +31,8 @@ class Interface:
         s3_parameters: s3p.S3Parameters = src.s3.s3_parameters.S3Parameters(connector=connector).exc()
         service: sr.Service = src.functions.service.Service(
             connector=connector, region_name=s3_parameters.region_name).exc()
+
+        # Setting up the cloud storage area
+        src.preface.setup.Setup(service=service, s3_parameters=s3_parameters).exc()
 
         return connector, s3_parameters, service
