c5954f2421f49e774afdbe0ed24613650336b192
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-05 22:43:51 +0100

    Update src/elements/master.py

--- src/elements/master.py
+++ src/elements/master.py
@@ -7,6 +7,13 @@ import datasets
 class Master(typing.NamedTuple):
     """
     The data type class ⇾ Master
+
+    id2label : dict
+        The codes & names of the fine grain labels of the FEW NERD (Few-Shot Named Entity Recognition Dataset) dataset
+    label2id : dict
+        The inverse of the above
+    data: datasets.DatasetDict
+        The FEW NERD (Few-Shot Named Entity Recognition Dataset) dataset
     """
 
     id2label: dict
