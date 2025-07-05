5d8a961ef0b64e1066cfc5e13b13c555d039e151
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-05 16:08:18 +0100

    Update src/algorithms/preference.py

--- src/algorithms/preference.py
+++ src/algorithms/preference.py
@@ -1,15 +1,27 @@
+"""Module preference.py"""
 import pandas as pd
 
 
 class Preference:
+    """
+    Sets up the labels in focus
+    """
 
     def __init__(self, arguments: dict):
+        """
+
+        :param arguments:
+        """
 
         self.__preference: list = arguments.get('preference')
         self.__rename: dict = arguments.get('rename')
 
 
     def __call__(self):
+        """
+        
+        :return:
+        """
 
         # A data frame consisting of the labels of interest
         frame = pd.DataFrame.from_records(data=list(enumerate(self.__preference)), columns=['identifier', 'name'])
