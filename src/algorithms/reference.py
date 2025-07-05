a710875017db9ea4a9e3c705635ddd21d625fdcf
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-05 15:39:52 +0100

    Update src/algorithms/reference.py

--- src/algorithms/reference.py
+++ src/algorithms/reference.py
@@ -1,8 +1,32 @@
 
+import pandas as pd
+
+
 class Reference:
 
-    def __init__(self):
-        pass
+    def __init__(self, id2label: dict):
 
-    def __call__(self, id2label: dict):
-        pass
+        self.__id2label = id2label
+
+
+    def __get_frame(self):
+
+        frame = pd.DataFrame.from_dict(self.__id2label, orient='index')
+        frame.reset_index(drop=False, inplace=True)
+        frame.rename(columns={'index': 'code', 0: 'name'}, inplace=True)
+
+        return frame
+
+    def __call__(self):
+        """
+
+        :return:
+        """
+        
+        frame = self.__get_frame()
+
+        splits = frame['name'].str.split(pat='-', n=1, expand=True)
+        splits.rename(columns={0: 'parent', 1: 'branch'}, inplace=True)
+
+
+        reference = frame.copy().join(splits, how='left')
