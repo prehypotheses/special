8ff9922b14bcfeae037d52965df064eab6bd9d93
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-05 15:45:26 +0100

    Update src/algorithms/reference.py

--- src/algorithms/reference.py
+++ src/algorithms/reference.py
@@ -8,7 +8,6 @@ class Reference:
 
         self.__id2label = id2label
 
-
     def __get_frame(self):
 
         frame = pd.DataFrame.from_dict(self.__id2label, orient='index')
@@ -22,11 +21,14 @@ class Reference:
 
         :return:
         """
-        
+
+        # Get the data frame of Fine NER Tags, i.e., the contents of self.__id2label as a data frame.
         frame = self.__get_frame()
 
+        # The data frame `frame` has 2 columns, `code` & `name`.  Each name is a concatenation
+        # of a Coarse NER Tag and a branch of the coarse tag.
         splits = frame['name'].str.split(pat='-', n=1, expand=True)
         splits.rename(columns={0: 'parent', 1: 'branch'}, inplace=True)
 
 
-        reference = frame.copy().join(splits, how='left')
+        return frame.copy().join(splits, how='left')
