bf78d4119c89e83388a6061a1f42072a1ab19d9e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-05 17:29:53 +0100

    Update src/algorithms/interface.py

--- src/algorithms/interface.py
+++ src/algorithms/interface.py
@@ -1,9 +1,9 @@
 
 import src.elements.master as mr
 
-
 class Interface:
 
-    def __init__(self, master: mr.Master):
+    def __init__(self, master: mr.Master, arguments: dict):
 
         self.__master = master
+        self.__arguments = arguments
